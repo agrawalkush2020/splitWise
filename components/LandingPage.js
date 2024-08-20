1714eb7ae03f7317e3905d3bc55c40590e21861d
kush-agr <kushagra.agrawal@creditas.com>
2024-08-21 01:16:50 +0530

    20 done

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,13 +1,51 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Input from "./Input";
 
-const LandingPage = ()=>{
-    return(
+const LandingPage = ({
+
+}) => {
+
+    const [number, setNumber] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleTheSubmit = (event) => {
+        event.preventDefault(); 
+    }
+
+    const handleSignIn = ()=>{
+        debugger
+    }
+
+    return (
         <div>
             <div>
-                <div>Log-In</div>
-                <div>Click to Sign-up</div>
+                <div>
+                    <form>
+                        <Input
+                            name={"Number"}
+                            type={'number'}
+                            label={'Number:'}
+                            value={number}
+                            placeHolder={'Number'}
+                            handleChange={setNumber}
+                        />
+                        <Input
+                            name={"password"}
+                            type={"password"}
+                            label={"password:"}
+                            value={password}
+                            placeHolder={"Password"}
+                            handleChange={setPassword}
+                        />
+                        <button className="bg-orange-600" type="submit" onClick={(event)=>handleTheSubmit(event)}>Submit</button>
+                    </form>
+                </div>
+                <div>Click to <a className="color-red cursor-pointer" onClick={handleSignIn}>Sign-Up</a></div>
+
             </div>
         </div>
     )
-
 }
+
+export default LandingPage;
