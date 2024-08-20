2a0c81ebc2a26fab7d01986ac6ea2969fbc4e162
kush-agr <kushagra.agrawal@creditas.com>
2024-08-20 23:40:29 +0530

    splitwise started

---
+++ components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LandingPage = ()=>{
+    return(
+        <div>
+            <div>
+                <div>Log-In</div>
+                <div>Click to Sign-up</div>
+            </div>
+        </div>
+    )
+
+}
